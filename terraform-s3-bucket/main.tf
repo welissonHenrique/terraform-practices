6f5240d6c5a0b39f8ae0bec0f465342fa0422244
wellhenrique <welissonh80@gmail.com>
2023-11-13 10:18:35 -0300

    chore: fixed terraform version and terraform provider

--- terraform-s3-bucket/main.tf
+++ terraform-s3-bucket/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = "1.6.3"
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.25.0"
+    }  
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
